c0a09687344fd69e0076e324cc4894074740ec3c
alessiovietri <info@alessiovietri.it>
2023-09-27 11:30:03 +0200

    Completata implementazione API

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Post;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        $posts = Post::paginate(3);
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'results' => $post
+            ]);
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Not found'
+            ]);
+        }
+    }
+
+}
